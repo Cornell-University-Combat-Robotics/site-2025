50a0d8e64d3ab2d3cdad6d50b87b7173b69f3474
Javohir Abdurazzakov <ja688@cornell.edu>
2024-11-12 19:46:46 -0500

    finished sponsor page

--- react-site/src/pages/sponsors.jsx
+++ react-site/src/pages/sponsors.jsx
@@ -1,4 +1,5 @@
-import { Box, Button, Typography, Grid2, Link } from "@mui/material";
+import { Box, Button, Typography, Grid2, Link, Accordion, AccordionSummary, AccordionDetails, Divider } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import team from "../assets/team-photo-hearts.png";
 import platLogo from "../assets/crc_plat.png";
 import asmlLogo from "../assets/asml.png";
@@ -12,11 +13,11 @@ import angelLogo from "../assets/Angel_Shot_Logo.png"
 import silverLogo from "../assets/crc_silver.png"
 import boeingLogo from "../assets/boeing.png"
 import bronzeLogo from "../assets/crc_bronze.png"
-import polarisLogo from "../assets/polaris_3.png"
-import empireLogo from "../assets/empire-plastics.png"
-import paragonLogo from "../assets/paragon_2.png"
 import purpleLogo from "../assets/crc_purple.png"
 import redbullLogo from "../assets/redbull.png"
+import daylightLogo from "../assets/daylight.png"
+import raymondLogo from "../assets/raymond.png"
+import sendcutsendLogo from "../assets/sendcutsend.png"
 
 export default function Sponsors() {
   return (
@@ -96,7 +97,7 @@ export default function Sponsors() {
               </Link>
             </Grid2>
             <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
+              <Link href="https://www.kfm-llc.com/">
                 <Box
                   component="img"
                   src={kfmLogo}
@@ -106,7 +107,7 @@ export default function Sponsors() {
               </Link>
             </Grid2>
             <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
+              <Link href="https://www.nhrl.io/">
                 <Box
                   component="img"
                   src={nhlrLogo}
@@ -116,7 +117,7 @@ export default function Sponsors() {
               </Link>
             </Grid2>
             <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
+              <Link href="https://www.autodesk.com/">
                 <Box
                   component="img"
                   src={autodeskLogo}
@@ -169,7 +170,7 @@ export default function Sponsors() {
           {/* Left side: Sponsor logos Grid2 */}
           <Grid2 container spacing={3} ml="90px" sx={{ maxWidth: '60%' }}>
             <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
+              <Link href="https://www.engineering.cornell.edu/">
                 <Box
                   component="img"
                   src={cornellEngLogo}
@@ -179,22 +180,42 @@ export default function Sponsors() {
               </Link>
             </Grid2>
             <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
-                <Box
-                  component="img"
-                  src={keenLogo}
-                  alt="Keen Insites Logo"
-                  sx={{ width: "450px", height: 'auto' }}
-                />
-              </Link>
-            </Grid2>
-            <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
+              <Link href="https://angelshotapp.com/">
                 <Box
                   component="img"
                   src={angelLogo}
                   alt="Angel Shot Logo"
                   sx={{ width: '200px', height: 'auto' }}
+                />
+              </Link>
+            </Grid2>
+            <Grid2 item xs={6} sm={3}>
+              <Link href="https://www.keeninsites.com/">
+                <Box
+                  component="img"
+                  src={keenLogo}
+                  alt="Keen Insites Logo"
+                  sx={{ width: "400px", height: 'auto' }}
+                />
+              </Link>
+            </Grid2>
+            <Grid2 item xs={6} sm={3}>
+              <Link href="https://daylightsavings.us/">
+                <Box
+                  component="img"
+                  src={daylightLogo}
+                  alt="The Daylight Savings Company Logo"
+                  sx={{ width: '400px', height: 'auto' }}
+                />
+              </Link>
+            </Grid2>
+            <Grid2 item xs={6} sm={3}>
+              <Link href="https://www.raymondcorp.com/">
+                <Box
+                  component="img"
+                  src={raymondLogo}
+                  alt="Raymond Logo"
+                  sx={{ width: '350px', height: 'auto' }}
                 />
               </Link>
             </Grid2>
@@ -245,7 +266,7 @@ export default function Sponsors() {
           {/* Right side: Sponsor logos Grid2 */}
           <Grid2 container spacing={3} mx="auto" sx={{ maxWidth: '60%' }}>
             <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
+              <Link href="https://www.boeing.com/">
                 <Box
                   component="img"
                   src={boeingLogo}
@@ -296,34 +317,13 @@ export default function Sponsors() {
             }}
           />
           {/* Left side: Sponsor logos Grid2 */}
-          <Grid2 container spacing={3} ml="90px" sx={{ maxWidth: '60%' }}>
+          <Grid2 container spacing={3} mx="auto" my="auto" sx={{ maxWidth: '60%' }}>
             <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
+              <Link href="https://sendcutsend.com/">
                 <Box
                   component="img"
-                  src={polarisLogo}
-                  alt="Polaris Logo"
-                  sx={{ width: '350px', height: 'auto' }}
-                  mb="-100px"
-                />
-              </Link>
-            </Grid2>
-            <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
-                <Box
-                  component="img"
-                  src={empireLogo}
-                  alt="Empite Plastics Logo"
-                  sx={{ width: "180px", height: 'auto' }}
-                />
-              </Link>
-            </Grid2>
-            <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
-                <Box
-                  component="img"
-                  src={paragonLogo}
-                  alt="Paragon Logo"
+                  src={sendcutsendLogo}
+                  alt="SendCutSend Logo"
                   sx={{ width: '350px', height: 'auto' }}
                 />
               </Link>
@@ -346,7 +346,7 @@ export default function Sponsors() {
       </Box>
 
       {/* Partner Logos Section with Purple Rectangle */}
-      <Box display="flex" justifyContent="center" alignItems="center" mt={5}>
+      <Box display="flex" justifyContent="center" alignItems="center" mt={5} mb={5}>
         <Box
           display="flex"
           justifyContent="space-between"
@@ -373,9 +373,9 @@ export default function Sponsors() {
           </Box>
 
           {/* Right side: Sponsor logos Grid2 */}
-          <Grid2 container spacing={3} ml="90px" sx={{ maxWidth: '60%' }}>
+          <Grid2 container spacing={3} ml="90px" alignItems="center" justifyContent="center" sx={{ maxWidth: '60%' }}>
             <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
+              <Link href="https://www.redbull.com/us-en">
                 <Box
                   component="img"
                   src={redbullLogo}
@@ -385,7 +385,7 @@ export default function Sponsors() {
               </Link>
             </Grid2>
             <Grid2 item xs={6} sm={3}>
-              <Link href="https://www.asml.com/en">
+              <Link style={{ textDecoration: 'none', color: "white" }} href="https://www.cee.cornell.edu/cee/facilities/bovay-laboratory-complex">
                 <Typography sx={{ fontFamily: 'Montserrat', fontSize: "2.5rem" }}> BOVAY LABORATORY COMPLEX </Typography>
               </Link>
             </Grid2>
@@ -402,6 +402,128 @@ export default function Sponsors() {
               backgroundColor: '#8307E4', // Light purple color
             }}
           />
+        </Box>
+      </Box>
+      <Box
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor="#63080A"
+        mt={5}
+        sx={{
+          color: 'white',
+          paddingY: '40px',
+          paddingX: '20px',
+          width: '100%',
+          margin: '0 auto',
+          boxSizing: 'border-box',
+        }}
+      >
+        <Box width="100%" justifyContent="center" alignItems="center">
+          <Typography variant="h4" align="center" sx={{ fontFamily: 'Josefin Sans', mb: 4 }}>
+            FAQ
+          </Typography>
+
+          <Box width="75%" justifyContent="center" alignItems="center" mx="auto">
+            {/* Accordion Items */}
+            <>
+              <Accordion
+                sx={{
+                  backgroundColor: '#63080A',
+                  color: 'white',
+                  boxShadow: 'none',
+                  width: '100%',
+                }}
+              >
+                <AccordionSummary expandIcon={<ExpandMoreIcon sx={{ color: 'white' }} />}>
+                  <Typography sx={{ fontStyle: 'italic', fontFamily: 'Josefin Sans' }}>
+                    I am a company, how can I sponsor?
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Via mail by filling out the donation form listed on the Sponsorship Packet above, or electronically donate through Cornell (shown above), or donate on Giving Day (shown above). Please email us to notify you wish to become a sponsor.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Divider sx={{ backgroundColor: 'white', my: 2 }} />
+
+              <Accordion
+                sx={{
+                  backgroundColor: '#63080A',
+                  color: 'white',
+                  boxShadow: 'none',
+                  width: '100%',
+                }}
+              >
+                <AccordionSummary expandIcon={<ExpandMoreIcon sx={{ color: 'white' }} />}>
+                  <Typography sx={{ fontStyle: 'italic', fontFamily: 'Josefin Sans' }}>
+                    I am an individual (not a company), how can I donate?
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    You can donate through the same platforms a company would: via mail by filling out the donation form listed on the Sponsorship Packet, or electronically donate through Cornell, or donate on Giving Day.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Divider sx={{ backgroundColor: 'white', my: 2 }} />
+
+              <Accordion
+                sx={{
+                  backgroundColor: '#63080A',
+                  color: 'white',
+                  boxShadow: 'none',
+                  width: '100%',
+                }}
+              >
+                <AccordionSummary expandIcon={<ExpandMoreIcon sx={{ color: 'white' }} />}>
+                  <Typography sx={{ fontStyle: 'italic', fontFamily: 'Josefin Sans' }}>
+                    I am a company and want to renew or update my sponsorship packet, what is the process?
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Donate through the same portal listed above, and email us to notify you wish to renew or update your sponsorship.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Divider sx={{ backgroundColor: 'white', my: 2 }} />
+
+              <Accordion
+                sx={{
+                  backgroundColor: '#63080A',
+                  color: 'white',
+                  boxShadow: 'none',
+                  width: '100%',
+                }}
+              >
+                <AccordionSummary expandIcon={<ExpandMoreIcon sx={{ color: 'white' }} />}>
+                  <Typography sx={{ fontStyle: 'italic', fontFamily: 'Josefin Sans' }}>
+                    I am a company and would like to make an arrangement not listed on the Sponsorship Packet, what should I do?
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Email us and we would be happy to make arrangements!
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Divider sx={{ backgroundColor: 'white', my: 2 }} />
+            </>
+          </Box>
+
+          {/* Footer */}
+          <Typography
+            align="center"
+            sx={{
+              fontStyle: 'italic',
+              fontFamily: 'Josefin Sans',
+              mt: 4,
+              color: 'white',
+            }}
+          >
+            If you have any further questions, please email us at combatrobotics@cornell.edu.
+          </Typography>
         </Box>
       </Box>
     </Box>
