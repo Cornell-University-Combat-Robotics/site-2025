9d9c9229499121cbd811b46f2135284199ace110
evzhu <ejzhu8512@gmail.com>
2024-11-16 13:01:28 -0500

    Improved IndividualRobotProps and the IndividualRobot component

--- react-site/src/components/IndividualRobot.tsx
+++ react-site/src/components/IndividualRobot.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Grid2, Typography, Card, CardMedia, Paper, List, ListItem, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
 import React from "react";
 
 /* **For each individual robot page, you will need to add it to 'App.jsx'. This is so our app recognizes the path to the page and can render 
@@ -10,28 +10,144 @@ import React from "react";
 export interface IndividualRobotProps {
   name: string; // Name of the robot
   makers: string[]; // Members who worked on the robot
-  desc: string; // Short description of the bot
+  description: string; // Short description of the bot
   image: string; // Link to the file location of the primary robot location
-  stats: string[]; // Index 0: Year, Index 1: Height, Index 2: Weight, Index 3: Fights, Index 4: Wins, Index 5: Top Speed, Index 6: Weapon Speed,
+  //stats: string[]; // Index 0: Year, Index 1: Height, Index 2: Weight, Index 3: Fights, Index 4: Wins, Index 5: Top Speed, Index 6: Weapon Speed,
   // Index 7: Most damaged part, Index 8: Favorite movie
+  stats: {
+    year: string;
+    height: string;
+    weight: string;
+    fights: string;
+    wins: string;
+    top_speed: string;
+    weapon_speed: string;
+    most_damaged_part: string;
+    favorite_movie: string;
+  }
   featured_fight: string; // Youtube link
   design: string;
   trivia: string[];
-  fights: string[][]; // In each fight array, Index 0: Event, Index 1: Opponent, Index 2: Result, Index 3: By, Index 4: Video, Index 5: Length (sec)
+  //fights: string[][]; // In each fight array, Index 0: Event, Index 1: Opponent, Index 2: Result, Index 3: By, Index 4: Video, Index 5: Length (sec)
+  fights: {
+    event: string;
+    opponent: string;
+    result: string;
+    by: string;
+    video: string;
+    length: string;
+  }[];
   gallery_imgs: string[]; // Link to the file location of pictures
   gallery_vids: string[]; // Link to the file location of videos
 }
 
 export default function IndividualRobot(props: IndividualRobotProps) {
   return (
-    <Box ml={15} mt={7} style={{ textAlign: 'left' }}>
+    <Box sx={{ p: 3 }}>
+      <Box sx={{ textAlign: 'left', ml: 15 }}>
+        {/* Header Section */}
+        <Typography variant='h2' gutterBottom>
+          {props.name}
+        </Typography>
+        <Typography variant="h4" align="left" width='80%' style={{ fontStyle: 'italic' }}>
+          {props.makers.join(', ')}
+        </Typography>
+
+        {/* Description & Featured Section */}
+        <Grid2 container spacing={3} sx={{ mt: 4, mb: 4 }}>
+          <Grid item xs={12} md={8}>
+            <Typography variant='h5'>Description</Typography>
+            <Typography mb={5}>Robot desciption...</Typography>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/CCGriVVoWeM?start=28006"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Paper sx={{ bgcolor: '#820002', p: 2 }}>
+              <Typography variant="h5" color="white" gutterBottom>
+                Stats
+              </Typography>
+              <List>
+                {Object.entries(props.stats).map(([key, value]) => (
+                  <ListItem key={key} sx={{ color: "white" }}>
+                    <strong>{key.replace(/([A-Z])/g, " $1")}: </strong>
+                    {value}
+                  </ListItem>
+                ))}
+              </List>
+            </Paper>
+          </Grid>
+        </Grid2>
+        
+        {/* Design Section */}
+        <Typography variant="h4" gutterBottom>Design</Typography>
+        <Typography variant="body1">{props.description}</Typography>
+
+        {/* Trivia Section */}
+        <Box sx={{ mt: 4 }}>
+          <Typography variant="h4" gutterBottom>
+            Trivia
+          </Typography>
+          <List sx={{ listStyleType: 'disc'}}>
+            {props.trivia.map((fact, index) => (
+              <ListItem key={index} sx={{ display: 'list-item' }}>{fact}</ListItem>
+            ))}
+          </List>
+        </Box>
+        
+        {/* Fights Section */}
+        <Box sx={{ mb: 4 }}>
+          <Typography variant="h4" gutterBottom>
+            Fights
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Event</TableCell>
+                  <TableCell>Opponent</TableCell>
+                  <TableCell>Result</TableCell>
+                  <TableCell>By</TableCell>
+                  <TableCell>Video</TableCell>
+                  <TableCell>Length (sec)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {props.fights.map((fight, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{fight.event}</TableCell>
+                    <TableCell>{fight.opponent}</TableCell>
+                    <TableCell>{fight.result}</TableCell>
+                    <TableCell>{fight.by}</TableCell>
+                    <TableCell>
+                      <a href={fight.video} target="_blank" rel="noopener noreferrer">
+                        Watch
+                      </a>
+                    </TableCell>
+                    <TableCell>{fight.length}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Box>
+    </Box>
+    
+    /* <Box ml={15} mt={7} style={{ textAlign: 'left' }}>
       <Typography variant="h2" align="left">{props.name}</Typography>
-      {/* **Include the nickname */}
+      {/* **Include the nickname
       <Typography variant="h4" align="left" width='80%' style={{ fontStyle: 'italic' }}>
         {props.makers.join(', ')}
       </Typography>
       <Typography variant="h5" mt={5}>Description</Typography>
-      {props.desc}
+      {props.description}
       <Typography variant="h5" mt={5}>Featured Fight</Typography>
       <iframe
         width="560"
@@ -50,6 +166,6 @@ export default function IndividualRobot(props: IndividualRobotProps) {
       {props.fights}
       <Typography variant="h5" mt={5}>Gallery</Typography>
 
-    </Box>
-  );
+    </Box> */
+  )
 }
--- react-site/src/pages/robots/beater.tsx
+++ react-site/src/pages/robots/beater.tsx
@@ -6,15 +6,50 @@ export default function Beater() {
     <IndividualRobot
       name="Beater B. Barker"
       makers={["Anna Boese", "Isaac Newcomb", "Stella Taglich", "Zander Knight", "James LaSalle", "Ethan Hernandez", "Kevin Sturm", "Mattew Anazco", "Mohammed Chowdhury"]}
-      desc="Beater B. Barker? I hardly know her! With his big 🅱 energy and his quick footwork on the arena floor, Beater absolutely dominates the competition. However, his one fatal flaw is the hubris of having a flywheel: he thinks he can turn on a dime, but he has to lean on the wall for 10 seconds if he's overzealous. This can (and has) cost him the match. He weeps when he remembers that day, but he knows there are more Beater Barkers out there in the S🅱iderverse, and one of them will win the championship when the time comes."
+      description="Beater B. Barker? I hardly know her! With his big 🅱 energy and his quick footwork on the arena floor, Beater absolutely dominates the competition. However, his one fatal flaw is the hubris of having a flywheel: he thinks he can turn on a dime, but he has to lean on the wall for 10 seconds if he's overzealous. This can (and has) cost him the match. He weeps when he remembers that day, but he knows there are more Beater Barkers out there in the S🅱iderverse, and one of them will win the championship when the time comes."
       image="/src/assets/nardo-whole.png"
-      stats={["Wins: 3", "Losses: 1", "KOs: 2"]}
-      featured_fight="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      stats={{
+        year: "2024",
+        height: "1.5 ft",
+        weight: "30 lbs",
+        fights: "3",
+        wins: "2",
+        top_speed: "11 mph",
+        weapon_speed: "1000 rpm",
+        most_damaged_part: "Weapon",
+        favorite_movie: "The Bee Movie"
+      }}
+      featured_fight="https://www.youtube.com/embed/CCGriVVoWeM?start=28006"
       design="A counter-rotating brass flywheel sits in the center of Beater's bright red beater bar. Powered separately, the flywheel and weapon counteract each other's angular momentum to reduce the gyro effect when Beater turns in place. This means he can pack the punch of a heavy hitter without getting bogged down by maneuverability problems.
       Speaking of maneuverability, Beater maximizes grip and speed with its titanium-cleated 4-wheel indirect drivetrain. With a top speed of 11 mph and an acceleration to full speed in a fraction of a second, there's no outrunning this bot. UHMW wheel guards keep the belts and pulleys protected, and 1/2 inch aluminum standoffs allow for quick, easy service.
       Also highly serviceable is the electronics compartment, housed in a UHMW billet frame and extending upward into a 3D - printed carbon - fiber nylon shell.The weapon uprights sandwich the electronics, held together by titanium rings that bolt in from the top and bottom."
       trivia={["Trivia 1", "Trivia 2"]}
-      fights={[["Fight 1", "Fight 2"], ["", ""]]}
+      fights={[
+        {
+          event: "Event 1",
+          opponent: "Opponent 1",
+          result: "Win",
+          by: "KO",
+          video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+          length: "2:30"
+        },
+        {
+          event: "Event 2",
+          opponent: "Opponent 2",
+          result: "Loss",
+          by: "KO",
+          video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+          length: "3:00"
+        },
+        {
+          event: "Event 3",
+          opponent: "Opponent 3",
+          result: "Win",
+          by: "Decision",
+          video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+          length: "3:30"
+        }
+      ]}
       gallery_imgs={["https://i.imgur.com/7Vz9JZ2.png", "https://i.imgur.com/7Vz9JZ2.png"]}
       gallery_vids={["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"]}
     />
