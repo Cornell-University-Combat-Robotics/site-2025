1a0a622b9735450e27f31a69a82398bde621f872
KingKlaus22 <137643935+KingKlaus22@users.noreply.github.com>
2025-01-29 20:07:59 -0500

    Create structure for open applications and timeline container

--- react-site/src/pages/apply.jsx
+++ react-site/src/pages/apply.jsx
@@ -4,7 +4,8 @@ import apply from "../assets/background-pictures/newbies-photo.jpg";
 /** Apply creates the Apply page for the website. */
 export default function Apply() {
   return (
-    <Box>
+    <Box sx={{}}>
+      {/* above is body of site */}
       <Box sx={{
         width: '100%',
         height: '100vh',
@@ -27,6 +28,76 @@ export default function Apply() {
           }}>
           JOIN US!
         </Typography>
+      </Box>
+        <Typography 
+        // Our applications...h3 writing
+          variant="h3"
+          sx={{
+            textAlign: 'center',
+            color: 'white',
+            margin: '1em 2em'
+          }}>
+          Our applications for Fall 2024 are open! All applications are due October 17th, 11:59PM
+        </Typography>
+        <Typography 
+        // Recruitment Timeline h1
+          variant="h1"
+          sx={{
+            textAlign: 'center',
+            color: 'white',
+            // add margins when text size smaller
+          }}>
+          RECRUITMENT TIMELINE
+        </Typography>
+      <Box sx={{
+        // timeline container
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'lime',
+          // backgroundImage: `url(${apply})`,
+          // backgroundSize: 'cover',
+          // backgroundPosition: 'top',
+          // backgroundRepeat: 'no-repeat',
+          // position: 'relative',
+      }}>
+        {/* timeline sections container */}
+        <Box sx={{backgroundColor: 'purple'}}>
+          <Typography variant="h4" sx={{color:"white"}}>
+            Hi
+          </Typography>
+          {/* next few boxes contain stuff for making timeline */}
+        </Box>
+        <Box sx={{backgroundColor: 'purple'}}>
+          {/* box */}
+          <Typography variant="h4" sx={{color:"white"}}>
+            Hi
+          </Typography>
+        </Box>
+        <Box sx={{backgroundColor: 'purple'}}>
+          {/* box */}
+          <Typography variant="h4" sx={{color:"white"}}>
+            Hi
+          </Typography>
+        </Box>
+        <Box sx={{backgroundColor: 'purple'}}>
+          {/* next few boxes contain stuff for making timeline */}
+          <Typography variant="h4" sx={{color:"white"}}>
+            Hi
+          </Typography>
+        </Box>
+        <Box sx={{backgroundColor: 'purple'}}>
+          {/* box */}
+          <Typography variant="h4" sx={{color:"white"}}>
+            Hi
+          </Typography>
+        </Box>
+        <Box sx={{backgroundColor: 'purple'}}>
+          {/* box */}
+          <Typography variant="h4" sx={{color:"white"}}>
+            Hi
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
