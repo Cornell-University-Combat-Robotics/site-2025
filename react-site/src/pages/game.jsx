85a9937c28b479832d0abc5d2a627c0aedc6f834
KingKlaus22 <137643935+KingKlaus22@users.noreply.github.com>
2025-03-01 12:14:57 -0500

    Make box div screen follow png animation to keep box in same proportion as png screen

--- react-site/src/pages/game.jsx
+++ react-site/src/pages/game.jsx
@@ -53,26 +53,49 @@ export default function Game() {
         // backgroundColor: "yellow"
       }}
     >
-      <Box 
-        sx={{
-          backgroundColor:"yellow",
-          // width: "1020px",
-          width: "80.4vh",
-          // height: "560px",
-          height: "56.6vh",
-          marginTop: "290px",
-          marginLeft: "20px",
-          zIndex: "10",
-          }}>
-      </Box>
+      <motion.div
+        initial={{ scale: 0.3 }}
+        animate={{ scale: 0.85, y: "45px" }}
+        transition={{ duration: 1, delay: 0.5, ease: [0.5, 1, 0.8, 1] }}
+        style={{ position: "absolute", zIndex: 0}}
+      >
+        <Box 
+          sx={{
+            backgroundColor:"yellow",
+            // width: "1020px",
+            width: "80.4vh",
+            // height: "560px",
+            height: "56.6vh",
+            marginTop: "290px",
+            marginLeft: "20px",
+            // zIndex: "3",
+            // zIndex doesnt serve purpose anymore since its wrapped in the motion div now
+            }}>
+        </Box>
+
+      </motion.div>
+      
       {/* Arcade Image as Background */}
       <motion.div
         initial={{ scale: 0.3 }}
-        animate={{ scale: 0.85, y: "10px" }}
+        animate={{ scale: 0.85, y: "45px" }}
         transition={{ duration: 1, delay: 0.5, ease: [0.5, 1, 0.8, 1] }}
         style={{ position: "absolute", zIndex: -1}}
       >
         <img src={arcade} style={{ width: "auto", height: "100%" }} />
+        {/* <Box 
+          sx={{
+            backgroundColor:"yellow",
+            // width: "1020px",
+            width: "200px",
+            // height: "560px",
+            height: "200px",
+            marginTop: "290px",
+            marginLeft: "20px",
+            // zIndex: "3",
+            // zIndex doesnt serve purpose anymore since its wrapped in the motion div now
+            }}>
+        </Box> */}
       </motion.div>
 
       {/* Title */}
