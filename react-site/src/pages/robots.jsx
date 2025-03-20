40905f628d7a983a8bd9393fb54f3d1c2cd6dbeb
Angela Z <87206794+angelavzhu@users.noreply.github.com>
2025-03-19 20:07:28 -0400

    Revert "Robot animation"

--- react-site/src/pages/robots.jsx
+++ react-site/src/pages/robots.jsx
@@ -1,106 +1,40 @@
 import nardo_whole from '../assets/nardo-whole.png';
-import { Box, Container, Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import RobotCard from '../components/RobotCard';
 import RobotCardList from '../components/RobotCardList';
-import StickyBox from '../components/StickyBox';
 
 export default function Robots() {
   return (
-    <Container maxWidth="md">
-      <Box display="flex" flexDirection="column" alignItems="center">
-        <StickyBox />
-        <Typography variant="h1" sx={{ fontFamily: 'Josefin Sans', mt: 5 }}>
-          ROBOTS
+    <Box sx={{padding: '10em 3em 1em 3em'}}>
+      {/* top right bottom left */}
+      <img src={nardo_whole} style={{ width: '70%', height: 'auto' }} />
+      <Typography variant="h1" sx={{ fontFamily: 'Josefin Sans', mt: 5 }}>
+        ROBOTS
+      </Typography>
+      <Typography variant="h3" sx={{ fontFamily: 'Josefin Sans', mt: 15 }}>
+        UPCOMING ROBOTS
+      </Typography>
+      
+      <Box display='flex' justifyContent='center' alignItems='center' sx={{ mt: 5 }}>
+        <Typography width='75%' sx={{ fontFamily: 'Josefin Sans' }}>
+          Hi ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
+          Proin at fringilla lacus, vulputate viverra nisl. Phasellus et nibh sed metus varius fringilla in eu nulla.
+          Donec convallis eros id eros elementum gravida. Phasellus at pulvinar lacus.
+          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+          Aenean ut ipsum vel massa condimentum feugiat. Donec at molestie nisi.
+          Suspendisse suscipit arcu vitae venenatis tempor. Duis non arcu sem. Donec sit amet ultricies mi, id varius eros.
+          Integer elementum lobortis mi vitae varius.
         </Typography>
-        <Typography variant="h3" sx={{ fontFamily: 'Josefin Sans', mt: 15 }}>
-          UPCOMING ROBOTS
-        </Typography>
+      </Box>
 
-        <Box display='flex' justifyContent='center' alignItems='center' sx={{ mt: 5 }}>
-          <Typography width='75%' sx={{ fontFamily: 'Josefin Sans' }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
-            Proin at fringilla lacus, vulputate viverra nisl. Phasellus et nibh sed metus varius fringilla in eu nulla.
-            Donec convallis eros id eros elementum gravida. Phasellus at pulvinar lacus.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-            Aenean ut ipsum vel massa condimentum feugiat. Donec at molestie nisi.
-            Suspendisse suscipit arcu vitae venenatis tempor. Duis non arcu sem. Donec sit amet ultricies mi, id varius eros.
-            Integer elementum lobortis mi vitae varius.
-          </Typography>
-        </Box>
+      {/* the bottom sections from here to "Retired Bots"
+          use flex and a function to map the cards to the
+          correct location in mobile view */}
 
-        <Stack direction='row' spacing={5} sx={{ mt: 5 }}>
-          <img src={nardo_whole} style={{ width: '50%', height: 'auto' }} />
-          <Stack spacing={2} mt='10'>
-            <Typography variant='h4' align='left' sx={{ fontFamily: 'Josefin Sans' }}>Rimshot | Kinetic</Typography>
-            <Typography variant='body' align='left' width='80%' sx={{ fontFamily: 'Josefin Sans' }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
-              Proin at fringilla lacus, vulputate viverra nisl. Phasellus et nibh sed metus varius fringilla in eu nulla.
-              Donec convallis eros id eros elementum gravida. Phasellus at pulvinar lacus.
-              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-              Aenean ut ipsum vel massa condimentum feugiat. Donec at molestie nisi.
-              Suspendisse suscipit arcu vitae venenatis tempor. Duis non arcu sem. Donec sit amet ultricies mi, id varius eros.
-              Integer elementum lobortis mi vitae varius.
-            </Typography>
-          </Stack>
-        </Stack>
-
-        <Stack direction='row' spacing={5} sx={{ mt: 5 }}>
-          <Stack spacing={2} mt='10'>
-            <Typography variant='h4' align='left' sx={{ fontFamily: 'Josefin Sans' }}>Capsize | Sportsman</Typography>
-            <Typography variant='body' align='left' width='80%' sx={{ fontFamily: 'Josefin Sans' }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
-              Proin at fringilla lacus, vulputate viverra nisl. Phasellus et nibh sed metus varius fringilla in eu nulla.
-              Donec convallis eros id eros elementum gravida. Phasellus at pulvinar lacus.
-              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-              Aenean ut ipsum vel massa condimentum feugiat. Donec at molestie nisi.
-              Suspendisse suscipit arcu vitae venenatis tempor. Duis non arcu sem. Donec sit amet ultricies mi, id varius eros.
-              Integer elementum lobortis mi vitae varius.
-            </Typography>
-          </Stack>
-          <img src={nardo_whole} style={{ width: '50%', height: 'auto' }} />
-        </Stack>
-
-        <Stack direction='row' spacing={5} sx={{ mt: 5 }}>
-          <img src={nardo_whole} style={{ width: '50%', height: 'auto' }} />
-          <Stack spacing={2} mt='10'>
-            <Typography variant='h4' align='left' sx={{ fontFamily: 'Josefin Sans' }}>Huey | Autonomous</Typography>
-            <Typography variant='body' align='left' width='80%' sx={{ fontFamily: 'Josefin Sans' }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
-              Proin at fringilla lacus, vulputate viverra nisl. Phasellus et nibh sed metus varius fringilla in eu nulla.
-              Donec convallis eros id eros elementum gravida. Phasellus at pulvinar lacus.
-              Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-              Aenean ut ipsum vel massa condimentum feugiat. Donec at molestie nisi.
-              Suspendisse suscipit arcu vitae venenatis tempor. Duis non arcu sem. Donec sit amet ultricies mi, id varius eros.
-              Integer elementum lobortis mi vitae varius.
-            </Typography>
-          </Stack>
-        </Stack>
-
-        <Typography variant="h3" sx={{ fontFamily: 'Josefin Sans', mt: 15 }}>
-          RETIRED ROBOTS
-        </Typography>
-
-        <Box display='flex' justifyContent='center' alignItems='center' sx={{ mt: 5 }}>
-          <Typography width='75%' sx={{ fontFamily: 'Josefin Sans' }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
-            Proin at fringilla lacus, vulputate viverra nisl. Phasellus et nibh sed metus varius fringilla in eu nulla.
-            Donec convallis eros id eros elementum gravida. Phasellus at pulvinar lacus.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-            Aenean ut ipsum vel massa condimentum feugiat. Donec at molestie nisi.
-            Suspendisse suscipit arcu vitae venenatis tempor. Duis non arcu sem. Donec sit amet ultricies mi, id varius eros.
-            Integer elementum lobortis mi vitae varius.
-          </Typography>
-        </Box>
-
-        <RobotCardList type='Retired' capacity={3} />
-
-        <Typography variant="h3" sx={{ fontFamily: 'Josefin Sans', mt: 15 }}>
-          3 LB ROBOTS
-        </Typography>
-
-        <Box display='flex' justifyContent='center' alignItems='center' sx={{ mt: 5 }}>
-          <Typography width='75%' sx={{ fontFamily: 'Josefin Sans' }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
+      <Box display="flex" flexDirection="column" alignItems="center" sx={{ px: 2 }}>
+        <Box display="flex" justifyContent="center" alignItems="center" sx={{ mt: 5, width: '100%' }}>
+          <Typography width="100%" maxWidth="800px" sx={{ fontFamily: 'Josefin Sans', textAlign: 'center' }}>
+            Hi ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
             Proin at fringilla lacus, vulputate viverra nisl. Phasellus et nibh sed metus varius fringilla in eu nulla.
             Donec convallis eros id eros elementum gravida. Phasellus at pulvinar lacus.
             Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
@@ -137,10 +71,25 @@ export default function Robots() {
           </Stack>
         ))}
       </Box>
-        <RobotCardList type='Three_lb' capacity={4} />
 
+
+
+      <Typography variant="h3" sx={{ fontFamily: 'Josefin Sans', mt: 15 }}>
+        RETIRED ROBOTS
+      </Typography>
+
+      <Box display='flex' justifyContent='center' alignItems='center' sx={{ mt: 5 }}>
+        <Typography width='75%' sx={{ fontFamily: 'Josefin Sans' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
+          Proin at fringilla lacus, vulputate viverra nisl. Phasellus et nibh sed metus varius fringilla in eu nulla.
+          Donec convallis eros id eros elementum gravida. Phasellus at pulvinar lacus.
+          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+          Aenean ut ipsum vel massa condimentum feugiat. Donec at molestie nisi.
+          Suspendisse suscipit arcu vitae venenatis tempor. Duis non arcu sem. Donec sit amet ultricies mi, id varius eros.
+          Integer elementum lobortis mi vitae varius.
+        </Typography>
       </Box>
-    </Container>
+
       <RobotCardList type='Retired' capacity={3} />
 
       <Typography variant="h3" sx={{ fontFamily: 'Josefin Sans', mt: 15 }}>
