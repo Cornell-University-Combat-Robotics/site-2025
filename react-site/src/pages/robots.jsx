2b0c6c5a0b21f9e45a0bef3009c7330d359a31ed
angelavzhu <angelzhua@gmail.com>
2025-03-19 20:17:45 -0400

    Fixing robot page

--- react-site/src/pages/robots.jsx
+++ react-site/src/pages/robots.jsx
@@ -15,7 +15,6 @@ export default function Robots() {
         <Typography variant="h3" sx={{ fontFamily: 'Josefin Sans', mt: 15 }}>
           UPCOMING ROBOTS
         </Typography>
-
         <Box display='flex' justifyContent='center' alignItems='center' sx={{ mt: 5 }}>
           <Typography width='75%' sx={{ fontFamily: 'Josefin Sans' }}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
@@ -27,7 +26,6 @@ export default function Robots() {
             Integer elementum lobortis mi vitae varius.
           </Typography>
         </Box>
-
         <Stack direction='row' spacing={5} sx={{ mt: 5 }}>
           <img src={nardo_whole} style={{ width: '50%', height: 'auto' }} />
           <Stack spacing={2} mt='10'>
@@ -43,7 +41,6 @@ export default function Robots() {
             </Typography>
           </Stack>
         </Stack>
-
         <Stack direction='row' spacing={5} sx={{ mt: 5 }}>
           <Stack spacing={2} mt='10'>
             <Typography variant='h4' align='left' sx={{ fontFamily: 'Josefin Sans' }}>Capsize | Sportsman</Typography>
@@ -59,7 +56,6 @@ export default function Robots() {
           </Stack>
           <img src={nardo_whole} style={{ width: '50%', height: 'auto' }} />
         </Stack>
-
         <Stack direction='row' spacing={5} sx={{ mt: 5 }}>
           <img src={nardo_whole} style={{ width: '50%', height: 'auto' }} />
           <Stack spacing={2} mt='10'>
@@ -75,11 +71,9 @@ export default function Robots() {
             </Typography>
           </Stack>
         </Stack>
-
         <Typography variant="h3" sx={{ fontFamily: 'Josefin Sans', mt: 15 }}>
           RETIRED ROBOTS
         </Typography>
-
         <Box display='flex' justifyContent='center' alignItems='center' sx={{ mt: 5 }}>
           <Typography width='75%' sx={{ fontFamily: 'Josefin Sans' }}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
@@ -91,13 +85,10 @@ export default function Robots() {
             Integer elementum lobortis mi vitae varius.
           </Typography>
         </Box>
-
         <RobotCardList type='Retired' capacity={3} />
-
         <Typography variant="h3" sx={{ fontFamily: 'Josefin Sans', mt: 15 }}>
           3 LB ROBOTS
         </Typography>
-
         <Box display='flex' justifyContent='center' alignItems='center' sx={{ mt: 5 }}>
           <Typography width='75%' sx={{ fontFamily: 'Josefin Sans' }}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
@@ -109,59 +100,8 @@ export default function Robots() {
             Integer elementum lobortis mi vitae varius.
           </Typography>
         </Box>
-
-        {[...Array(3)].map((_, index) => (
-          <Stack
-            key={index}
-            direction={{ xs: 'column', sm: index % 2 === 0 ? 'row' : 'row-reverse' }}
-            spacing={5}
-            sx={{ mt: 5, width: '100%', maxWidth: '1200px' }}
-          >
-            <Box sx={{ flex: 1 }}>
-              <img
-                src={nardo_whole}
-                alt="Example"
-                style={{ width: '100%', height: 'auto', borderRadius: '8px' }}
-              />
-            </Box>
-            <Stack spacing={2} sx={{ flex: 1 }}>
-              <Typography variant="h4" align="left" sx={{ fontFamily: 'Josefin Sans' }}>
-                {index === 0 ? 'Rimshot | Kinetic' : index === 1 ? 'Capsize | Sportsman' : 'Huey | Autonomous'}
-              </Typography>
-              <Typography variant="body1" align="left" sx={{ fontFamily: 'Josefin Sans', lineHeight: 1.6 }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
-                Proin at fringilla lacus, vulputate viverra nisl. Phasellus et nibh sed metus varius fringilla in eu nulla.
-                Donec convallis eros id eros elementum gravida.
-              </Typography>
-            </Stack>
-          </Stack>
-        ))}
-      </Box>
         <RobotCardList type='Three_lb' capacity={4} />
-
       </Box>
     </Container>
-      <RobotCardList type='Retired' capacity={3} />
-
-      <Typography variant="h3" sx={{ fontFamily: 'Josefin Sans', mt: 15 }}>
-        3 LB ROBOTS
-      </Typography>
-
-      <Box display='flex' justifyContent='center' alignItems='center' sx={{ mt: 5 }}>
-        <Typography width='75%' sx={{ fontFamily: 'Josefin Sans' }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur nisl quis diam iaculis ultricies.
-          Proin at fringilla lacus, vulputate viverra nisl. Phasellus et nibh sed metus varius fringilla in eu nulla.
-          Donec convallis eros id eros elementum gravida. Phasellus at pulvinar lacus.
-          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-          Aenean ut ipsum vel massa condimentum feugiat. Donec at molestie nisi.
-          Suspendisse suscipit arcu vitae venenatis tempor. Duis non arcu sem. Donec sit amet ultricies mi, id varius eros.
-          Integer elementum lobortis mi vitae varius.
-        </Typography>
-      </Box>
-
-      <RobotCardList type='Three_lb' capacity={4} />
-      {/* capacity indicates how many cards per row */}
-
-    </Box>
   );
 }
