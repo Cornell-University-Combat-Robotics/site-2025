081c840a40290f0d410e4e736331bcedc71dea4e
TVijay2021 <tejusvi.vijay@gmail.com>
2026-09-15 18:57:17 -0400

    Updated home page timeline images and events

--- src/data/timeline1.ts
+++ src/data/timeline1.ts
@@ -1,13 +1,17 @@
-import { Timeline1Cell } from "../components/Timeline1Cell";
+import type { Timeline1Cell } from "../components/Timeline1Cell";
+
 import panel1 from "../assets/home-timeline/panel-1.jpeg";
 import panel2 from "../assets/home-timeline/panel-2.jpg";
 import panel3 from "../assets/home-timeline/panel-3.jpg";
 import panel4 from "../assets/home-timeline/panel-4.jpg";
 import panel5 from "../assets/home-timeline/panel-5.jpg";
 import panel6 from "../assets/home-timeline/panel-6.jpg";
-import panel7 from "../assets/home-timeline/panel-7.jpg";
 import panel8 from "../assets/home-timeline/panel-8.jpg";
 import panel9 from "../assets/home-timeline/panel-9.png";
+import champs22_panel from "../assets/home-timeline/champs22_panel.jpeg";
+import fullteam_panel from "../assets/home-timeline/fullteam_photo.jpeg";
+import maycomp_panel from "../assets/home-timeline/may-comp-panel.JPG";
+import febcomp_panel from "../assets/home-timeline/feb-comp-photo.JPG";
 
 
 const TimelineData: Timeline1Cell[] = [
@@ -23,7 +27,6 @@ const TimelineData: Timeline1Cell[] = [
     },
     {
         date: 'January 2022',
-
         description: 'We received project team status from Cornell, allowing us to set up shop in the Experiential Learning Lab (ELL) and provide higher standards of member education.',
         image: panel3
     },
@@ -38,14 +41,14 @@ const TimelineData: Timeline1Cell[] = [
         image: panel5
     },
     {
+        date: 'December 2022',
+        description: 'We qualified for our first-ever NHRL World Championship event, traveling to the 2022 World Championships with our 12lb vertical spinner robot, Richard.',
+        image: champs22_panel
+    },
+    {
         date: 'April 2023',
         description: 'We participated in the Bits of Our Mind (BOOM) event and showcased our Arcade Video Game, CRC VS.',
         image: panel6
-    },
-    {
-        date: 'May 2023',
-        description: 'We hosted our second Showcase for the Cornell community, this time in Duffield Atrium! We demonstrated Autonomous\' facial recognition algorithm with Lance.',
-        image: panel7
     },
     {
         date: 'October 2023',
@@ -54,10 +57,23 @@ const TimelineData: Timeline1Cell[] = [
     },
     {
         date: 'April 2024',
-        description: 'We attended our fourth competition at NHRL, where Kinetic\'s Beater B. Barker, Sportsman\'s Nardo, and Autonomous\' Louis XVI all competed. Beater achieved 5th place!',
+        description: 'We debuted our first autonomous robot (Louis XVII), becoming one of the first teams in NHRL history to compete with fully autonomous combat robotics.',
         image: panel9
     },
-
+    {
+        date: 'August 2025',
+        description: 'Both our Sportsman and Kinetic subteams began developing kinetic-weapon robots, expanding our team\'s capabilities in high-energy combat robotics.',
+        image: fullteam_panel
+    },    
+    {
+        date: 'February 2026',
+        description: 'We competed at our first-ever February NHRL event, making this our first season with two NHRL competitions.',
+        image: febcomp_panel
+    },
+    {
+        date: 'August 2026',
+        description: 'Our Sportsman and Kinetic teams merged into one Mechanical Subteam.',
+        image: maycomp_panel
+    },
 ];
 export default TimelineData;
-
